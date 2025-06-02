0fdccde6bafa6120e30d16bf0c41ac31104649e6
Alfie D <alfie.day@diegesis.co.uk>
2025-06-02 14:35:14 +0100

    add exercise 3

--- react_app/src/TaskList.jsx
+++ react_app/src/TaskList.jsx
@@ -4,11 +4,34 @@
 
 // Import the Task component so we can use it
 import Task from './Task'
+// Import useState from React - this is a hook that lets us add state to our component
+import { useState } from 'react'
 
 function TaskList() {
-  // This is our array of tasks
-  // In a real app, this might come from a database or API
-  const tasks = ['Learn React', 'Build a To-Do App', 'Test app'];
+  // useState is a React Hook that lets us add state to our component
+  // The first value (tasks) is our state variable
+  // The second value (setTasks) is a function to update our state
+  // The initial value is an array with two tasks
+  const [tasks, setTasks] = useState(['Learn React', 'Build a To-Do App']);
+  
+  // This state will hold the current value of our input field
+  const [newTask, setNewTask] = useState('');
+
+  // This function will be called when the form is submitted
+  const handleSubmit = (e) => {
+    // Prevent the default form submission behavior
+    e.preventDefault();
+    
+    // Only add the task if it's not empty
+    if (newTask.trim() !== '') {
+      // Use the setTasks function to update our tasks array
+      // We use the spread operator (...) to keep all existing tasks
+      // and add our new task at the end
+      setTasks([...tasks, newTask]);
+      // Clear the input field after adding the task
+      setNewTask('');
+    }
+  };
 
   // The return statement is where we define what this component will render
   // We can only return one root element (in this case, the outer <div>)
@@ -19,6 +42,22 @@ function TaskList() {
       {/* h1 is a heading element that will display "My Tasks" */}
       <h1>My Tasks</h1>
       
+      {/* This is our form for adding new tasks */}
+      <form onSubmit={handleSubmit}>
+        {/* 
+          This input field is controlled by our newTask state
+          When the user types, the onChange event updates our state
+          The value is always what's in our state
+        */}
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <button type="submit">Add Task</button>
+      </form>
+
       {/* This is our unordered list that will contain our tasks */}
       <ul>
         {/* 
